2a7f711a15daa50ea6e80e4c7ef10ed028ff023a
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-02-13 17:42:33 +0300

    Add form

---
+++ main.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('#form');
+const mainHolder = document.querySelector('.container');
+const bookInput = document.querySelector('#book');
+const authorInput = document.querySelector('#author');
+const buttonAdd = document.querySelector('#add');
+
+let books = [];
+
+function show() {
+  const books = JSON.parse(localStorage.getItem('books'));
+  for (let index = 0; index < books.length; index += 1) {
+    bookInput.value = books[index].book;
+    authorInput.value = books[index].author;
+  }
+}
+form.addEventListener('load', () => {
+  show();
+});
+
+if (localStorage.getItem('books')) {
+  books = JSON.parse(localStorage.getItem('books'));
+}
+
+function addBook(book, author) {
+  const newBook = { book, author };
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+function displayBooks() {
+  mainHolder.innerHTML = '';
+  for (let i = 0; i < books.length; i += 1) {
+    const bookHolder = document.createElement('section');
+    bookHolder.innerHTML = `
+      <h3>${books[i].author}<h3>
+      <p>${books[i].book}</p>
+      <button id="remove">remove</button>
+      <hr>
+       `;
+    mainHolder.appendChild(bookHolder);
+  }
+}
+
+buttonAdd.addEventListener('click', (e) => {
+  e.preventDefault();
+  const book = bookInput.value;
+  const author = authorInput.value;
+  addBook(book, author);
+  displayBooks();
+});
+
+displayBooks();

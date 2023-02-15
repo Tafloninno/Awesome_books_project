c7b9e404e1b8a408c9fd1a5e866952676c10426b
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-02-15 13:35:54 +0300

    add navigation bar

--- main.js
+++ main.js
@@ -5,71 +5,76 @@ const authorInput = document.querySelector('#author');
 const buttonAdd = document.querySelector('#add');
 
 class Books {
-  constructor(title, author) {
+  constructor(title, author){
     this.title = title;
     this.author = author;
+    this.id = Math.floor(Math.random() * 1000000);
   }
 }
 
 class SavelocalItems {
-  static getBook() {
+   static getBook(){
     let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
+    if(localStorage.getItem('books') === null){
+      books = []
+    }else {
+      books = JSON.parse(localStorage.getItem('books'))
+    } 
     return books;
-  }
+   }
+   static saveItem(book){
+      const books = SavelocalItems.getBook()
+      books.push(book)
+      localStorage.setItem('books', JSON.stringify(books))
+   }
 
-  static saveItem(book) {
+  static removeBook(id) {
     const books = SavelocalItems.getBook();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBooks(author) {
-    const books = SavelocalItems.getBook();
-    const updatedBooks = books.filter((book) => book.author !== author);
+    const updatedBooks = books.filter(book => book.id !== id);
     localStorage.setItem('books', JSON.stringify(updatedBooks));
   }
 }
 
 class ListBooks {
-  static displaybooks() {
-    const booksInserted = SavelocalItems.getBook();
-    booksInserted.forEach((book) => ListBooks.addBooks(book));
+  static displaybooks(){
+    const booksInserted = SavelocalItems.getBook()
+    booksInserted.forEach(book => ListBooks.addBook(book))
   }
 
-  static addBooks(book) {
-    const bookHolder = document.createElement('tr');
+  static addBook(book) {
+    let bookHolder = document.createElement('tr')
     bookHolder.innerHTML = `
-    <td>''${book.title}'' <span> by </span> ${book.author}</td>
-    <button><a href="#" class="remove">Remove</button>
-      `;
-    bookList.appendChild(bookHolder);
+      <td>''${book.title}'' by ${book.author}</td>
+      <td><button class="remove" data-id="${book.id}">Remove</button></td>
+    `
+    bookList.appendChild(bookHolder)
   }
 
-  static DeleteBook(item) {
-    if (item.classList.contains('remove')) {
-      const books = item.parentElement.previousElementSibling.textContent.split('by')[1].trim();
-      item.parentElement.parentElement.remove();
-      SavelocalItems.removeBooks(books);
+  static deleteBook(id) {
+    const bookRow = document.querySelector(`[data-id="${id}"]`).closest('tr');
+    if (bookRow) {
+      bookRow.remove();
+      SavelocalItems.removeBook(id);
     }
   }
 }
 
 ListBooks.displaybooks();
 
-buttonAdd.addEventListener('click', (e) => {
+buttonAdd.addEventListener('click', (e)=>{
   e.preventDefault();
-  const title = bookInput.value;
-  const author = authorInput.value;
-  const book = new Books(title, author);
-  ListBooks.addBooks(book);
-  SavelocalItems.saveItem(book);
-});
+  let title = bookInput.value;
+  let author = authorInput.value;
+  const book = new Books(title, author)
+  ListBooks.addBook(book)
+  SavelocalItems.saveItem(book)
+  bookInput.value = '';
+  authorInput.value = '';
+})
 
-bookList.addEventListener('click', (e) => {
-  ListBooks.DeleteBook(e.target);
-});
+bookList.addEventListener('click',(e)=>{
+  if (e.target.classList.contains('remove')) {
+    const id = parseInt(e.target.dataset.id, 10);
+    ListBooks.deleteBook(id);
+  }
+})
